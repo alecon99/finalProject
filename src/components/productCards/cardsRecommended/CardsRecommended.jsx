f6141931f4fbfdee56dc844328c18b6c99db760f
alecon99 <alessioconte45@gmail.com>
2023-09-27 18:50:57 +0200

    ecommerce.V3.2

--- src/components/productCards/cardsRecommended/CardsRecommended.jsx
+++ src/components/productCards/cardsRecommended/CardsRecommended.jsx
@@ -1,19 +1,21 @@
 import { useState, useEffect } from 'react'
 
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row, Spinner } from 'react-bootstrap';
 
-import SingleCard from '../singleCard/SingleCard';
 import SingleCardRecommended from '../singleCardRecommended/SingleCardRecommended';
 
 const CardsRecommended = ({ category }) => {
 
     const [recommendedProducts, serRecommendedProducts] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
 
     const getRecommendedProduct = async () => {
         try {
+            setIsLoading(true)
             const data = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/recomendedProducts/${category}`);
             const response = await data.json();
             serRecommendedProducts(response.products);
+            setIsLoading(false)
         } catch (error) {
             console.log(error);
         }
@@ -25,15 +27,23 @@ const CardsRecommended = ({ category }) => {
 
     return (
         <Row >
-            {recommendedProducts[0] && recommendedProducts.map((product) => {
-                return (
-                    <Col key={product._id} xs={6} sm={4} lg={2}>
-                        <SingleCardRecommended
-                            productProps={product}
-                        />
-                    </Col>
-                )
-            })}
+            {isLoading ?
+                <div className='text-center'>
+                    <Spinner />
+                </div>
+                :
+                <>
+                    {recommendedProducts[0] && recommendedProducts.map((product) => {
+                        return (
+                            <Col key={product._id} xs={6} sm={4} lg={2}>
+                                <SingleCardRecommended
+                                    productProps={product}
+                                />
+                            </Col>
+                        )
+                    })}
+                </>
+            }
         </Row>
     )
 }
